6d8a2a3d8c9bcedbd6cc2172a20017731f7c6176
Carl-Magnus Lein <cale3010@gmail.com>
2020-10-10 10:05:00 +0200

    Adds radio buttons, missing funcionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataQuery } from "@dhis2/app-runtime";
+import { Radio } from "@dhis2/ui-core";
 import i18n from "@dhis2/d2-i18n";
 import styles from "./App.module.css";
 import Organisation from "./components/Organisation.js";
@@ -12,30 +13,73 @@ const query = {
   },
 };
 
-const MyApp = () => (
-  <div className={styles.container}>
-    <DataQuery query={query}>
-      {({ error, loading, data }) => {
-        console.log(data);
-        if (error) return <span>ERROR</span>;
-        if (loading) return <span>...</span>;
-        console.log(data.me.organisationUnits[0].id);
-        return (
-          <>
-            <div className={styles.menu}>
-              <UserInfo />
-              <CalendarComp />
-            </div>
-            {data.me.organisationUnits.map((organisation) => (
-              <>
-                <Organisation organisation={data.me.organisationUnits} />
-              </>
-            ))}
-          </>
-        );
-      }}
-    </DataQuery>
-  </div>
-);
+const MyApp = () => {
+  const [active, setActive] = useState(false);
+  return (
+    <div className={styles.container}>
+      <DataQuery query={query}>
+        {({ error, loading, data }) => {
+          console.log(data);
+          if (error) return <span>ERROR</span>;
+          if (loading) return <span>...</span>;
+          console.log(data.me.organisationUnits[0].id);
+          return (
+            <>
+              <div className={styles.menu}>
+                <UserInfo />
+                <Radio
+                  dataTest="dhis2-uicore-radio"
+                  label="Both"
+                  name="Ex"
+                  onChange={() => {
+                    setActive(true);
+                    console.log();
+                  }}
+                  value="default"
+                />{" "}
+                <Radio
+                  dataTest="dhis2-uicore-radio"
+                  label="Index"
+                  name="Ex"
+                  onChange={() => {
+                    setActive(true);
+                    console.log();
+                  }}
+                  value="default"
+                />{" "}
+                <Radio
+                  dataTest="dhis2-uicore-radio"
+                  label="Contacts"
+                  name="Ex"
+                  onChange={() => {
+                    setActive(true);
+                    console.log();
+                  }}
+                  value="default"
+                />{" "}
+                <Radio
+                  dataTest="dhis2-uicore-radio"
+                  label="Completed"
+                  name="Ex"
+                  onChange={() => {
+                    setActive(true);
+                    console.log();
+                  }}
+                  value="default"
+                />
+                <CalendarComp />
+              </div>
+              {data.me.organisationUnits.map((organisation) => (
+                <>
+                  <Organisation organisation={data.me.organisationUnits} />
+                </>
+              ))}
+            </>
+          );
+        }}
+      </DataQuery>
+    </div>
+  );
+};
 
 export default MyApp;
