50ff64600925e21d84b25e9fc01ca9c91d36d3a7
Carl-Magnus Lein <cale3010@gmail.com>
2020-11-05 16:46:32 +0100

    Organisation unit now choosen from app user, defaults if undefined

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { DataQuery } from "@dhis2/app-runtime";
 import styles from "./App.module.css";
-import  { Breakpoint, BreakpointProvider } from 'react-socks';
+import { Breakpoint, BreakpointProvider } from "react-socks";
 import RadioButtons from "./components/RadioButtons";
 import CasesList from "./components/CasesList";
 import CalendarComponent from "./components/CalendarComponent";
@@ -19,6 +19,7 @@ const query = {
 };
 
 /* Query for getting one index/contact case */
+
 const personQuery = {
   trackedEntityInstances: {
     resource: "trackedEntityInstances/QG0e3EvdHFp",
@@ -29,57 +30,77 @@ const personQuery = {
 };
 
 /* Query for getting all index cases */
-const indexQuery = {
-  trackedEntityInstances: {
-    resource: "trackedEntityInstances",
-    params: {
-      ou: "JwnjhjVgXP2",
-      program: "uYjxkTbwRNf",
-      programStatus: "ACTIVE",
-      programStage: "oqsk2Jv4k3s",
-      fields: "*",
+function indexQuery(orgUnit) {
+  let ou = orgUnit ? orgUnit : "JwnjhjVgXP2";
+  //TODO: Remove, this is for development
+  if (ou === "BPzJYNBjmwO") ou = "JwnjhjVgXP2";
+  return {
+    trackedEntityInstances: {
+      resource: "trackedEntityInstances/",
+      params: {
+        ou: ou,
+        program: "uYjxkTbwRNf",
+        programStatus: "ACTIVE",
+        programStage: "oqsk2Jv4k3s",
+        fields: "*",
+      },
     },
-  },
-};
+  };
+}
 
 /* Query for getting all contacts cases */
-const contactsQuery = {
-  trackedEntityInstances: {
-    resource: "trackedEntityInstances",
-    params: {
-      ou: "JwnjhjVgXP2",
-      program: "DM9n1bUw8W8",
-      programStatus: "ACTIVE",
-      programStage: "sAV9jAajr8x",
-      fields: "*",
+function contactsQuery(orgUnit) {
+  let ou = orgUnit ? orgUnit : "JwnjhjVgXP2";
+  //TODO: Remove, this is for development
+  if (ou === "BPzJYNBjmwO") ou = "JwnjhjVgXP2";
+  return {
+    trackedEntityInstances: {
+      resource: "trackedEntityInstances/",
+      params: {
+        ou: ou,
+        program: "DM9n1bUw8W8",
+        programStatus: "ACTIVE",
+        programStage: "sAV9jAajr8x",
+        fields: "*",
+      },
     },
-  },
-};
+  };
+}
 
 /* Query for getting all index/contact cases */
-const bothQuery = {
-  trackedEntityInstances: {
-    resource: "trackedEntityInstances",
-    params: {
-      ou: "JwnjhjVgXP2",
-      fields: "*",
+function bothQuery(orgUnit) {
+  let ou = orgUnit ? orgUnit : "JwnjhjVgXP2";
+  //TODO: Remove, this is for development
+  if (ou === "BPzJYNBjmwO") ou = "JwnjhjVgXP2";
+  return {
+    trackedEntityInstances: {
+      resource: "trackedEntityInstances/",
+      params: {
+        ou: ou,
+        fields: "*",
+      },
     },
-  },
-};
+  };
+}
 
 /* Query for getting all cases with status completed */
 //TODO: Make this get contact cases as well, need testdata to reflect this. We only have index cases with status completed.
-const completedQuery = {
-  trackedEntityInstances: {
-    resource: "trackedEntityInstances",
-    params: {
-      ou: "JwnjhjVgXP2",
-      program: "uYjxkTbwRNf",
-      programStatus: "COMPLETED",
-      fields: "*",
+function completedQuery(orgUnit) {
+  let ou = orgUnit ? orgUnit : "JwnjhjVgXP2";
+  //TODO: Remove, this is for development
+  if (ou === "BPzJYNBjmwO") ou = "JwnjhjVgXP2";
+  return {
+    trackedEntityInstances: {
+      resource: "trackedEntityInstances/",
+      params: {
+        ou: ou,
+        program: "uYjxkTbwRNf",
+        programStatus: "COMPLETED",
+        fields: "*",
+      },
     },
-  },
-};
+  };
+}
 
 const MyApp = () => {
   /* State for if any index case has its overview clicked, then shows modal */
@@ -92,90 +113,86 @@ const MyApp = () => {
   const [totalCases, setTotalCases] = useState();
 
   /* State for radio buttons */
-  const [clicked, setClicked] = useState("Index");
+  const [clicked, setClicked] = useState("Index"); //TODO: Add Caching, just presets for this.
 
-  //TODO: Add Caching, just presets for this.
   const [index, setIndex] = useState();
   const [contacts, setContacts] = useState();
   const [both, setBoth] = useState();
   const [completed, setCompleted] = useState();
-
   const [dateFilter, setDateFilter] = useState();
-
   return (
     <BreakpointProvider>
-    <div className={styles.container}>
-      <div className={styles.menu}>
-                  <UserInfo />
-                  <RadioButtons setClicked={setClicked} />
-                  <CalendarComponent
-                    dateRange={dateRange}
-                    setDateRange={setDateRange}
-                    setDateFilter={setDateFilter}
-                  />
-                  <TotalCases dateRange={dateRange} totalCases={totalCases} />
-                </div>
-      <div className={styles.table}>
-        <DataQuery query={query}>
-          {({ error, loading, data }) => {
-            if (error) return <Error />;
-            if (loading) return <Loader />;
-            console.log("Me:", data.me.organisationUnits[0]);
-            return (
-              <>
-                
+      <div className={styles.container}>
+        <div className={styles.menu}>
+          <UserInfo />
+          <RadioButtons setClicked={setClicked} />
+          <CalendarComponent
+            dateRange={dateRange}
+            setDateRange={setDateRange}
+            setDateFilter={setDateFilter}
+          />
+          <TotalCases dateRange={dateRange} totalCases={totalCases} />
+        </div>
+        <div className={styles.table}>
+          <DataQuery query={query}>
+            {({ error, loading, data }) => {
+              if (error) return <Error />;
+              if (loading) return <Loader />;
+              console.log("Me:", data.me);
+              return (
                 <>
-                  {clicked === "Index" && (
-                    <CasesList
-                      query={indexQuery}
-                      dateRange={dateRange}
-                      setTotalCases={setTotalCases}
-                      setClickedModal={setClickedModal}
-                      clikedCase={clicked}
-                      dateFilter={dateFilter}
-                    />
-                  )}
-                  {clicked === "Contacts" && (
-                    <CasesList
-                      query={contactsQuery}
-                      dateRange={dateRange}
-                      setTotalCases={setTotalCases}
-                      setClickedModal={setClickedModal}
-                      clikedCase={clicked}
-                      dateFilter={dateFilter}
-                    />
-                  )}
-                  {clicked === "Both" && (
-                    <CasesList
-                      query={bothQuery}
-                      dateRange={dateRange}
-                      setTotalCases={setTotalCases}
-                      setClickedModal={setClickedModal}
-                      clikedCase={clicked}
-                      dateFilter={dateFilter}
-                    />
-                  )}
-                  {clicked === "Completed" && (
-                    <CasesList
-                      query={completedQuery}
-                      dateRange={dateRange}
-                      setTotalCases={setTotalCases}
-                      setClickedModal={setClickedModal}
-                      clikedCase={clicked}
-                      dateFilter={dateFilter}
-                    />
-                  )}
+                  <>
+                    {clicked === "Index" && (
+                      <CasesList
+                        query={indexQuery(data.me.organisationUnits[0].id)}
+                        dateRange={dateRange}
+                        setTotalCases={setTotalCases}
+                        setClickedModal={setClickedModal}
+                        clikedCase={clicked}
+                        dateFilter={dateFilter}
+                      />
+                    )}
+                    {clicked === "Contacts" && (
+                      <CasesList
+                        query={contactsQuery(data.me.organisationUnits[0].id)}
+                        dateRange={dateRange}
+                        setTotalCases={setTotalCases}
+                        setClickedModal={setClickedModal}
+                        clikedCase={clicked}
+                        dateFilter={dateFilter}
+                      />
+                    )}
+                    {clicked === "Both" && (
+                      <CasesList
+                        query={bothQuery(data.me.organisationUnits[0].id)}
+                        dateRange={dateRange}
+                        setTotalCases={setTotalCases}
+                        setClickedModal={setClickedModal}
+                        clikedCase={clicked}
+                        dateFilter={dateFilter}
+                      />
+                    )}
+                    {clicked === "Completed" && (
+                      <CasesList
+                        query={completedQuery(data.me.organisationUnits[0].id)}
+                        dateRange={dateRange}
+                        setTotalCases={setTotalCases}
+                        setClickedModal={setClickedModal}
+                        clikedCase={clicked}
+                        dateFilter={dateFilter}
+                      />
+                    )}
+                  </>
                 </>
-              </>
-            );
-          }}
-        </DataQuery>
-        {clickedModal && (
-          <ContactModal id={clickedModal} setClickedModal={setClickedModal} />
-        )}
-        {console.log(clickedModal)}
+              );
+            }}
+          </DataQuery>
+          {clickedModal && (
+            <ContactModal id={clickedModal} setClickedModal={setClickedModal} />
+          )}
+          {console.log(clickedModal)}
+        </div>
       </div>
-    </div>
     </BreakpointProvider>
   );
 };
